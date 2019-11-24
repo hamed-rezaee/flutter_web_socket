c86f4b2dc5d30f30bc03b482c9deedc14059119f
Hamed <hamed@firstsource.io>
2019-11-24 18:46:44 +0330

    - initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text('Flutter Demo Home Page'),
+    );
+  }
+}
